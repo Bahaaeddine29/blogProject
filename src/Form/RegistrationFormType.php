a62c1fd4a8f15940d9560513e72052832a93dd99
Bahaaeddine <bahaaeddine.ghezz@gmail.com>
2023-07-07 15:26:02 +0200

    Register finis

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -5,9 +5,11 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -17,7 +19,23 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
+        ->add('firstName', null)
+        ->add('lastName', null)
+        ->add('email', EmailType::class, [
+            'constraints' => 
+                new Email ([
+                    'message' => 'Adresse email non valide',
+              ]),
+        ])
+        ->add('username', null, [
+            'constraints' =>
+            new Length([
+                'min' => 4,
+                'minMessage' => 'Username must be at least 4 characters',
+                // max length allowed by Symfony for security reasons
+                'max' => 25,
+            ]), 
+                ])
             ->add('agreeTerms', CheckboxType::class, [
                 'mapped' => false,
                 'constraints' => [
